c48273210678a9e6148f5a9789afe0efd877075e
AlexandreLuisBecker <didiao@yahoo.com>
2024-06-26 21:30:42 -0300

    1

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,6 @@
+package org.example;
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/org/example/CalculadoraTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class CalculadoraTest {
+    @Test
+    public void testSomar() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.somar(2, 3);
+        assertEquals(5, resultado);
+    }
+}
